c0319cabff8c8e59f83ed771ec9a41b7e031b4b1
yangxue <yangxue@cisco.com>
2018-11-26 10:21:20 +0800

    init

---
+++ asde/asde/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  asde
+//
+//  Created by yangxue on 2018/11/26.
+//  Copyright © 2018 cisco. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ asde/asde/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  asde
+//
+//  Created by yangxue on 2018/11/26.
+//  Copyright © 2018 cisco. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
